e87e7388e14568d732a7d12727e9d1e85f1b202b
siamsubekti <47563288+siamsubekti@users.noreply.github.com>
2023-06-19 14:25:55 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# swipe-gesture-swipe-2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/swipe-gesture-klwupq)
---
+++ app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import {
+  HammerGestureConfig,
+  HAMMER_GESTURE_CONFIG,
+} from '@angular/platform-browser';
+import { Hammer } from 'hammerjs';
+
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = <any>{
+    swipe: { direction: Hammer.DIRECTION_ALL, threshold: 5 },
+  };
+}
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig,
+    },
+  ],
+})
+export class AppComponent implements OnInit {
+  eventText = '';
+  deltaY: number;
+
+  ngOnInit(): void {
+    this.deltaY = document.getElementById('div-swipe').offsetTop;
+    console.log('delta y div swipe ', this.deltaY);
+  }
+
+  onSwipe(evt) {
+    console.log('y ', evt.deltaY);
+    this.deltaY = evt.deltaY;
+    const x =
+      Math.abs(evt.deltaX) > 40 ? (evt.deltaX > 0 ? 'right' : 'left') : '';
+    const y = Math.abs(evt.deltaY) > 40 ? (evt.deltaY > 0 ? 'down' : 'up') : '';
+
+    this.eventText += `${x} ${y}<br/>`;
+  }
+}
